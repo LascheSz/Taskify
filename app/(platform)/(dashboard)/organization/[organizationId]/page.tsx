6fdaaabd0b28678977611e38e1dafcf2cdf96886
LascheSz <elaschinsky10@gmail.com>
2023-11-22 11:56:43 +0100

    Create / Delete Board Action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,30 @@
-const OrganizationsPage = () => {
+import { Button } from "@/components/ui/button";
+import { create } from "./actions/create-board";
+import { db } from "@/lib/db";
+import { Board } from "./board";
+
+const OrganizationsPage = async() => {
+    const boards = await db.board.findMany();
+
     return (
-        <div>
-            Organization Page!
+        <div className="flex flex-col space-y-4">
+            <form action={create}>
+                <input 
+                id="title"
+                name="title"
+                required
+                placeholder="Board Title"
+                className="boarder-black border p-1"
+                />
+                <Button type="submit">
+                    Erstellen
+                </Button>
+            </form>
+            <div className="space-y-2">
+                {boards.map((board) => (
+                    <Board key={board.id} title={board.title} id={board.id} />
+                ))}
+            </div>
         </div>
     );
 };
