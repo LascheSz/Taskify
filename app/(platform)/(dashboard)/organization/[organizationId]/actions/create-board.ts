96a2bdc9c262d83ef00e02111bc883558d5f8159
LascheSz <elaschinsky10@gmail.com>
2023-11-22 12:32:30 +0100

    form.tsx error | create-board.ts

--- app/(platform)/(dashboard)/organization/[organizationId]/actions/create-board.ts
+++ app/(platform)/(dashboard)/organization/[organizationId]/actions/create-board.ts
@@ -4,22 +4,47 @@ import { z } from "zod";
 
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export type State = {
+    errors?: {
+        title?: string[];
+    }
+    message?: string | null;
+}
 
 const CreateBoard = z.object({
-    title: z.string(),
+    title: z.string().min(3, {
+        message: "Der Titel muss mindestens 3 zeichen lang sein"
+    })
 });
 
-export async function create(formData: FormData) {
-    const { title } = CreateBoard.parse({
+export async function create(prevState: State, formData: FormData) {
+    const validatedFields = CreateBoard.safeParse({
         title: formData.get("title"),
     });
-    
-    await db.board.create({
-        data: {
-            title,
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing Fields."
         }
-    });
+    }
+
+    const { title } = validatedFields.data;
+    
+    try {
+        await db.board.create({
+            data: {
+                title,
+            }
+        });
+    } catch (error) {
+    return {
+        message: "Database Error",
+    }
+}
 
     revalidatePath("/organization/org_2YRIxwiNAjY3cXKXN68pQ9aLdbH");
+    redirect("/organization/org_2YRIxwiNAjY3cXKXN68pQ9aLdbH");
 }
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,25 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { create } from "./actions/create-board";
 import { db } from "@/lib/db";
+
 import { Board } from "./board";
+import { Form } from "./form";
 
 const OrganizationsPage = async() => {
     const boards = await db.board.findMany();
 
     return (
         <div className="flex flex-col space-y-4">
-            <form action={create}>
-                <input 
-                id="title"
-                name="title"
-                required
-                placeholder="Board Title"
-                className="boarder-black border p-1"
-                />
-                <Button type="submit">
-                    Erstellen
-                </Button>
-            </form>
+            <Form />
             <div className="space-y-2">
                 {boards.map((board) => (
                     <Board key={board.id} title={board.title} id={board.id} />
